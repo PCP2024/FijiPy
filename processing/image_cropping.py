f17876f0c8063e60c2e3a25ee8ba68f91ba80d38
yota <yotakawashima@gmail.com>
2024-05-14 16:51:14 +0200

    move test part to test function.

--- processing/image_cropping.py
+++ processing/image_cropping.py
@@ -1,12 +1,9 @@
-#from dataio.image_loader import image_loader
-import cv2
 import json
 import os
 
 with open("data_file.json", "r") as read_file:
     data = json.load(read_file)
 
-################################################
 def image_cropping(image, crop_width, crop_height, crop_x=0, crop_y=0):
         """
         Crop image into a desired size.
@@ -29,17 +26,3 @@ def image_cropping(image, crop_width, crop_height, crop_x=0, crop_y=0):
         # Crop the image
         cropped_image = image[crop_y:crop_y+crop_height, crop_x:crop_x+crop_width]
         return cropped_image
-
-############################################
-# test the function above
-data_dir = '..' + os.sep + 'demodata'  + os.sep
-image_path = data_dir + 'demo_Image.jpg'
-image = cv2.imread(image_path) # Load the image
-#image = image_loader(image_path) # Load the image
-crop_width = 100
-crop_height = 100
-crop_x=100
-crop_y=100
-cropped_image = image_cropping(image, crop_width, crop_height, crop_x, crop_y)
-image_cropped_path = data_dir + 'demo_Image_cropped.jpg'
-cv2.imwrite(image_cropped_path, cropped_image)
