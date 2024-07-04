71eec63db7f4bea0fe75f2ce21c7f657432f8272
Bianca Ariani <106977422+biancaariani@users.noreply.github.com>
2024-07-04 12:30:27 +0200

    fixed shape mismatch btwn edge detect & saliency when calling image_2_midi, fixed saliency_map geenration, added progress bar for saliency_map

--- processing/edge_detection.py
+++ processing/edge_detection.py
@@ -19,14 +19,14 @@ def detect_edges(data: dict,image):
     """
     if isinstance(image, str):
         image = cv2.imread(image)
-
+    #print("Shape of image into edge detection: ", image.shape)
     # Convert the image to grayscale if it is not already
     if len(image.shape) > 2:
         image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     # Apply Canny edge detection
     edges = cv2.Canny(image, data['canny_lower_threshold'], data['canny_upper_threshold'])
-
+    #print("Shape of edge map: ", edges.shape)
     return edges
 
 ############################################
