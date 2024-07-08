35b162ff96fd6b3b3cc7759c10a57ca89dc599b7
yota <yotakawashima@gmail.com>
2024-07-08 18:07:33 +0200

    Fix IndentationError: unexpected indent

--- processing/image_cropping.py
+++ processing/image_cropping.py
@@ -28,21 +28,21 @@ def crop_image(data: dict, image: Union[str, np.ndarray]) -> np.ndarray:
         Cropped image.
 
     """
-        # either load the image from the path or use the image array
-        if isinstance(image, str):
-            image = cv2.imread(image)
+    # either load the image from the path or use the image array
+    if isinstance(image, str):
+        image = cv2.imread(image)
 
-        # Load hyperparameters from data json file
-        crop_width = data['crop_width']
-        crop_height = data['crop_height']
-        crop_x = data['crop_x']
-        crop_y = data['crop_y']
+    # Load hyperparameters from data json file
+    crop_width = data['crop_width']
+    crop_height = data['crop_height']
+    crop_x = data['crop_x']
+    crop_y = data['crop_y']
 
-        if image.shape[0] < crop_height or image.shape[1] < crop_width:
-            raise ValueError("The crop region is larger than the image.")
-        if image.shape[0] < crop_height + crop_y or image.shape[1] < crop_width + crop_x:
-            raise ValueError("The crop region is out of the image.")
-                
-        # Crop the image
-        cropped_image = image[crop_y:crop_y+crop_height, crop_x:crop_x+crop_width]
-        return cropped_image
+    if image.shape[0] < crop_height or image.shape[1] < crop_width:
+        raise ValueError("The crop region is larger than the image.")
+    if image.shape[0] < crop_height + crop_y or image.shape[1] < crop_width + crop_x:
+        raise ValueError("The crop region is out of the image.")
+            
+    # Crop the image
+    cropped_image = image[crop_y:crop_y+crop_height, crop_x:crop_x+crop_width]
+    return cropped_image
